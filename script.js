f2b2cf03014c194fda031bc513257ff1459fb152
Rishit Goyal <155085969+Rishitgoyal06@users.noreply.github.com>
2025-05-31 14:06:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+let count = document.getElementById('counter');
+let incrementButton = document.getElementById('incrementBtn');
+
+incrementButton.addEventListener('click', () => {
+    let currentCount = parseInt(counter.textContent);
+    alert(`Current value: ${currentCount}`);
+    counter.textContent = currentCount + 1;
+ });
